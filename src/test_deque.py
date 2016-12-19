36d81babfad8b4a4fff8de808dcc8c462fbb1aa1
Colin Lamont <colinlamont@gmail.com>
2016-12-19 14:24:08 -0800

    added append and appendleft tests

--- src/test_deque.py
+++ src/test_deque.py
@@ -31,3 +31,32 @@ def test_creating_a_filled_deque(create_filled_deque):
     assert create_filled_deque._container.head.value == 5
     assert create_filled_deque._container.tail.value == 1
     assert create_filled_deque._container._size == 5
+
+
+def test_append_adding_node_to_tail(create_empty_deque):
+    """Test adding a new node to the tail of an empty deque."""
+    create_empty_deque.append(6)
+    assert create_empty_deque._container.tail.value == 6
+    assert create_empty_deque._container.head.value == 6
+    assert create_empty_deque._container._size == 1
+
+
+def test_append_on_a_filled_deque_list(create_filled_deque):
+    """Test adding a new node to end of a filled deque."""
+    create_filled_deque.append(6)
+    assert create_filled_deque._container.tail.value == 6
+    assert create_filled_deque._container._size == 6
+
+
+def test_appendleft_on_an_empty_deque(create_empty_deque):
+    """Test adding a node to front of a deque."""
+    create_empty_deque.appendleft(6)
+    assert create_empty_deque._container.head.value == 6
+    assert create_empty_deque._container._size == 1
+
+
+def test_appendleft_on_a_filled_deque(create_filled_deque):
+    """Test adding a node to the front of a filled deque."""
+    create_filled_deque.appendleft(6)
+    assert create_filled_deque._container.head.value == 6
+    assert create_filled_deque._container._size == 6
