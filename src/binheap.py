995de38149aca4e4a21044de33d826d3903320e2
Ted Callahan <CCallahanIV@gmail.com>
2016-12-20 13:31:36 -0800

    Wrote basic structure of classes and first test.

--- src/binheap.py
+++ src/binheap.py
@@ -1,2 +1,28 @@
 """This module contains an implementation of a binary heap."""
 
+
+class TreeNode(object):
+    """Define a Node object for use in binary trees."""
+    def __init__(self, value=None, parent=None, left=None, right=None):
+        """Initialize a TreeNode Object."""
+        self.value = value
+        self.parent = parent
+        self.left = left
+        self.right = right
+
+
+class BinHeap(object):
+    """Define a Binary Heap Object."""
+    def __init__(self, maybe_an_iterable=None):
+        """Initialize a Binary Heap Object."""
+        self._tree_list = []
+        self._size = len(self._tree_list)
+
+    def pop(self):
+        """Pop the head of the heap off."""
+        pass
+
+    def push(self, value):
+        """Add a Node to the bottom of the heap with given value."""
+        pass
+
--- src/test_binheap.py
+++ src/test_binheap.py
@@ -1,1 +1,20 @@
 """This module contains the tests for the binary heap."""
+
+import pytest
+
+
+@pytest.fixture
+def create_empty_tree_node():
+    """create an empty tree node object."""
+    from binheap import TreeNode
+    node = TreeNode()
+    return node
+
+
+def test_create_empty_tree_node(create_empty_tree_node):
+    """Test creation of an empty Tree Node."""
+    node = create_empty_tree_node
+    assert node.value is None
+    assert node.parent is None
+    assert node.left is None
+    assert node.right is None
