23430ebb1a9d96635201c68903ffab90955418d8
Colin Lamont <colinlamont@gmail.com>
2016-12-21 08:43:49 -0800

    added tests, but failed to figure out how to do it.

--- src/test_binheap.py
+++ src/test_binheap.py
@@ -2,19 +2,41 @@
 
 import pytest
 
+PUSH_TABLE = [
+    [4.5, [1, 3, 5, 4, 6, 7, 9, 4.5]]
+    [3.5, [1, 3, 5, 3.5, 6, 7, 9, 4]]
+    [2.5, [1, 2.5, 5, 3, 6, 7, 9, 4]]
+    [0.5, [0.5, 1, 5, 3, 6, 7, 9, 4]]
+    [0, [1, 5, 3, 6, 7, 9, 4]]
+]
+
 
 @pytest.fixture
-def create_empty_tree_node():
-    """create an empty tree node object."""
-    from binheap import TreeNode
-    node = TreeNode()
-    return node
+def create_filled_bin_heap():
+    """Create a filled min bin heap."""
+    from binheap import BinHeap
+    filled_heap = BinHeap([1, 3, 5, 4, 6, 7, 9])
+    return filled_heap
 
 
-def test_create_empty_tree_node(create_empty_tree_node):
-    """Test creation of an empty Tree Node."""
-    node = create_empty_tree_node
-    assert node.value is None
-    assert node.parent is None
-    assert node.left is None
-    assert node.right is None
+def test_push_to_end_of_an_empty_bin_heap():
+    """Test pushing val to end of an empty bin heap."""
+    from binheap import BinHeap
+    new_list = BinHeap()
+    assert len(new_list.push(4)) == 1
+
+
+@pytest.mark.usefixtures("create_filled_bin_heap")
+@pytest.mark.parametrize("val, result", PUSH_TABLE)
+def test_push_to_end_of_a_filled_bin_heap(val, result):
+    """Test pushing val to end of a filled bin heap."""
+    new_filled_list = create_filled_bin_heap()
+    new_filled_list.push(val)
+    assert new_filled_list._tree_list == result
+
+
+def test_pop_off_bin_heap(create_filled_bin_heap):
+    """Test popping root value off the top of a filled bin heap."""
+    new_filled_list = create_filled_bin_heap()
+    new_filled_list.pop()
+    assert new_filled_list[0] == 3
