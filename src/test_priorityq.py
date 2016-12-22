da0c86768a4bfb79a6a90ef015da5d3ed61e7af5
Colin Lamont <colinlamont@gmail.com>
2016-12-21 16:58:30 -0800

    added a bunch of tests for the priority queue.

--- src/test_priorityq.py
+++ src/test_priorityq.py
@@ -27,21 +27,41 @@ def test_creation_of_empty_priority_q(empty_priority_q):
     assert len(empty_priority_q._high_p) == 0
     assert len(empty_priority_q._pdict) == 0
 
-def test_initialize_with_single_tuple
+def test_initialize_with_single_tuple():
+    """The test initializes priority q with a single tuple."""
+    from priorityq import PriorityQ
+    new_pq = PriorityQ((3,2))
+    assert len(new_pq) == 1
+    assert new_pq._high_p == 2
+    assert new_pq.peek() == 3
     
 
-def test_intitalize_with_single_digit
+def test_intitalize_with_single_digit():
+    """The test initialized a prio q with a single digit."""
+    from priorityq import PriorityQ
+    with pytest.raises(TypeError):
+        new_pq = PriorityQ(3)
 
-def test_into_empty_with_tuple_and_no_prio
 
-def test_into_empty_with_tuple_and_prio_not_already_there
+def test_insert_empty_with_val_and_no_prio(empty_priority_q):
+    """The test inserts val w/o prio to empty list."""
+    in_epq = empty_priority_q.insert(4)
+    assert in_epq._high_p == 0
+    assert in_epq._pdict[0].peek() == 4
+
+
+def test_insert_filled_with_val_and_prio_where_prio_not_already_there(filled_priority_q):
+    """The test inserts with val and prio, where prio not already there."""
+    in_fpq = filled_priority_q.insert(7, 4)
+    assert in_fpq._pdict[4].peek() == 7
+
 
 def insert_val_into_empty_priorty_q(empty_priority_q):
     """The tests inserting into an empty priority queue."""
     new_prq = empty_priority_q
-    new_prq.insert((1, 3))
+    new_prq.insert(3, 1)
     assert len(empty_priority_q) == 1
-    assert empty_priority_q._high_p[0] == 1
+    assert empty_priority_q._high_p[0] == 3
     assert empty_priority_q._pdict[0] == 3
 
 def test_insert_into_full_with_single_digit_and_no_prio(filled_priority_q):
