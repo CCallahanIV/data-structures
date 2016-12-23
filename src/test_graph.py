bddf1d14d0ae94ddafe63afa8002787b1b4b54a7
Ted Callahan <CCallahanIV@gmail.com>
2016-12-23 12:32:24 -0800

    Wrote tests for add_node()

--- src/test_graph.py
+++ src/test_graph.py
@@ -2,19 +2,52 @@
 
 import pytest
 
+TEST_NODES = [1, 2, 3, 4, "five", "six", "seven", "apple"]
+
 
 @pytest.fixture
-def create_graph():
+def empty_graph():
     """Create an empty graph."""
     from graph import Graph
     return Graph()
 
 
-def test_init_graph(create_graph):
+@pytest.fixture
+def graph_nodes():
+    from graph import Graph
+    g = Graph()
+    for node in TEST_NODES:
+        g.add_node(node)
+    return g
+
+
+def test_init_graph(empty_graph):
     """Test initialization of an empty graph."""
-    assert create_graph._gdict == {}
+    assert empty_graph._gdict == {}
 
 
-def test_nodes_returns_empty_list_empty_graph(create_graph):
+def test_nodes_returns_empty_list_empty_graph(empty_graph):
     """Test nodes() returns empty list for an empty graph."""
-    assert create_graph.nodes() == []
+    assert empty_graph.nodes() == []
+
+
+def test_nodes_returns_list_non_empty_graph(graph_nodes):
+    """Test nodes() returns correct list."""
+    node_list = graph_nodes.nodes()
+    for node in node_list:
+        assert node in TEST_NODES
+    assert len(node_list) == len(TEST_NODES)
+
+
+def test_add_node_creates_new_node(empty_graph):
+    """Test add_node() creates a new node."""
+    g = empty_graph
+    g.add_node(1)
+    assert g._gdict[1] == []
+
+
+def test_add_node_already_exists(empty_graph):
+    """Test adding a node that already exists."""
+    empty_graph.add_node(1)
+    empty_graph.add_node(1)
+    assert empty_graph._gdict[1] == []
