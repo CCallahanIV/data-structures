f92fff0ca49616f7c6ea925599e942d6a6869c0a
Ted Callahan <CCallahanIV@gmail.com>
2016-12-23 12:08:51 -0800

    Wrote first test for nodes.

--- src/test_graph.py
+++ src/test_graph.py
@@ -1,2 +1,20 @@
 """This module contains the tests for a graph data structure."""
 
+import pytest
+
+
+@pytest.fixture
+def create_graph():
+    """Create an empty graph."""
+    from graph import Graph
+    return Graph()
+
+
+def test_init_graph(create_graph):
+    """Test initialization of an empty graph."""
+    assert create_graph._gdict == {}
+
+
+def test_nodes_returns_empty_list_empty_graph(create_graph):
+    """Test nodes() returns empty list for an empty graph."""
+    assert create_graph.nodes() == []
