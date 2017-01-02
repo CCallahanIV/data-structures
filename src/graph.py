ee5bd5753979b159f1b5bb5adfc5c2a358d662cb
Ted Callahan <CCallahanIV@gmail.com>
2017-01-02 15:16:48 -0800

    Fixed non-ASCII issue.

--- src/graph.py
+++ src/graph.py
@@ -1,4 +1,5 @@
 """This module implements a graph data structure."""
+
 from queue_ds import Queue
 
 
@@ -9,12 +10,12 @@ class Graph(object):
 
         g.nodes(): return a list of all nodes in the graph
         g.edges(): return a list of all edges in the graph
-        g.add_node(n): adds a new node ‘n’ to the graph
-        g.add_edge(n1, n2): adds a new edge to the graph connecting ‘n1’ and ‘n2’, if either n1 or n2 are not already present in the graph, they should be added.
-        g.del_node(n): deletes the node ‘n’ from the graph, raises an error if no such node exists
-        g.del_edge(n1, n2): deletes the edge connecting ‘n1’ and ‘n2’ from the graph, raises an error if no such edge exists
-        g.has_node(n): True if node ‘n’ is contained in the graph, False if not.
-        g.neighbors(n): returns the list of all nodes connected to ‘n’ by edges, raises an error if n is not in g
+        g.add_node(n): adds a new node 'n' to the graph
+        g.add_edge(n1, n2): adds a new edge to the graph connecting 'n1' and 'n2', if either n1 or n2 are not already present in the graph, they should be added.
+        g.del_node(n): deletes the node 'n' from the graph, raises an error if no such node exists
+        g.del_edge(n1, n2): deletes the edge connecting 'n1' and 'n2' from the graph, raises an error if no such edge exists
+        g.has_node(n): True if node 'n' is contained in the graph, False if not.
+        g.neighbors(n): returns the list of all nodes connected to 'n' by edges, raises an error if n is not in g
         g.adjacent(n1, n2): returns True if there is an edge connecting n1 and n2, False if not, raises an error if either of the supplied nodes are not in g
 
     """
