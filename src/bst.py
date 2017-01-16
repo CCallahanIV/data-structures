0ecf7b2390632e399030028033f7c0cccbce0789
Colin Lamont <colinlamont@gmail.com>
2017-01-16 14:30:42 -0800

    added insert method.

--- src/bst.py
+++ src/bst.py
@@ -1,2 +1,29 @@
-
 """Module for Binary Search Tree."""
+
+
+class BinarySearchTree(object):
+    """Foo."""
+
+    def __init__(self):
+        """Init of the Binary Search Tree class."""
+        self._bstdict = {}
+        self.root = None
+        self.vertex = self.root
+
+    def insert(self, val):
+        """Takes a value, inserts into Binary Search Tree at correct placement."""
+        if not any(self._bstdict):
+            self.root = val
+            self._bstdict[val] = ["z", "z"]
+
+        elif val > self._bstdict[self.vertex]:
+            if self._bstdict[self.vertex][1] == "z":
+                self._bstdict[self.vertex][1] = val
+            self.vertex = self.vertex[1]
+            insert(val)
+
+        elif val < self._bstdict[self.vertex]:
+            if self._bstdict[self.vertex][0] == "z":
+                self._bstdict[self.vertex][0] = val
+            self.vertex = self.vertex[0]
+            insert(val)
