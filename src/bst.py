9a2285f67373531684c64445e64ee7041249917a
Ted Callahan <CCallahanIV@gmail.com>
2017-01-16 12:59:05 -0800

    Completed initial set up.

--- src/bst.py
+++ src/bst.py
@@ -1,14 +1,14 @@
 """This module implements a binary search tree."""
 
+
 class Node(object):
     """Node object for use in a binary search tree."""
+
     def __init__(self):
         self.key = key
         self.value = value
         self.right = right
         self.left = left
-
-
 
 
 class BinarySearchTree(object):
@@ -26,7 +26,30 @@ class BinarySearchTree(object):
 
     def __init__(self):
         """Initialize a Binary Search Tree object."""
-        pass
+        self.root = None
+        self.size = None
+        self.depth = None
+        self.balance = None
 
     def insert(self, val):
-      
+        """Insert a new node with val into the BST."""
+        pass
+
+    def search(self, val):
+        """Return the node with value val or return None."""
+        pass
+
+    def size(self):
+        """Return the integer size of the BST."""
+
+    def depth(self):
+        """Return the integer depth of the BST."""
+        pass
+
+    def contains(self, val):
+        """Return True if node with value val is in BST, False if not."""
+        pass
+
+    def balance(self):
+        """Return positive or negative integer that represents how well balanced the tree is."""
+        pass
