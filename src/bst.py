5b00c11ebfaeaf6e8dd2c2dc3bc976c36fbfd599
Ted Callahan <CCallahanIV@gmail.com>
2017-01-17 15:11:01 -0800

    Adding travis integration.

---
+++ src/bst.py
@@ -0,0 +1,190 @@
+"""This module implements a binary search tree."""
+
+from queue_ds import Queue
+from stack import Stack
+
+
+class Node(object):
+    """Node object for use in a binary search tree."""
+
+    def __init__(self, value, right=None, left=None):
+        """Initialize a node for a binary search tree object."""
+        self.value = value
+        self.right = right
+        self.left = left
+
+    def _has_children(self):
+        """Return True or False if Node has children."""
+        if self.right or self.left:
+            return True
+        return False
+
+    def _return_children(self):
+        """Return all children of a Node."""
+        if self.left and self.right:
+            return [self.left, self.right]
+        elif self.left or self.right:
+            return [self.left] if self.left else [self.right]
+
+
+class BinarySearchTree(object):
+    """Binary Search Tree Object.
+
+    Methods:
+      - insert(self, val): will insert the value val into the BST. If val is already present, it will be ignored.
+      - search(self, val): will return the node containing that value, else None
+      - size(self): will return the integer size of the BST (equal to the total number of values stored in the tree). It will return 0 if the tree is empty.
+      - depth(self): will return an integer representing the total number of levels in the tree. If there is one value, the depth should be 1, if two values it will be 2, if three values it may be 2 or three, depending, etc.
+      - contains(self, val): will return True if val is in the BST, False if not.
+      - balance(self): will return an integer, positive or negative that represents how well balanced the tree is. Trees which are higher on the left than the right should return a positive value, trees which are higher on the right than the left should return a negative value. An ideally-balanced tree should return 0.
+      - in_order(self): will return a generator that will return the values in the tree using in-order traversal, one at a time.
+      - pre_order(self): will return a generator that will return the values in the tree using pre-order traversal, one at a time.
+      - post_order(self): will return a generator that will return the values in the tree using post_order traversal, one at a time.
+      - breadth_first(self): will return a generator that will return the values in the tree using breadth-first traversal, one at a time.
+
+    """
+
+    def __init__(self):
+        """Initialize a Binary Search Tree object."""
+        self.root = None
+        self._size = 0
+
+    def insert(self, val):
+        """Insert a new node with val into the BST."""
+        # if not self.search(val):
+        #     return
+        new_node = Node(val)
+        if self.root is None:
+            self.root = new_node
+            self._size = 1
+            return
+
+        curr_node = self.root
+        while curr_node:
+            if val > curr_node.value:
+                if curr_node.right:
+                    curr_node = curr_node.right
+                else:
+                    curr_node.right = new_node
+                    self._size += 1
+                    break
+            elif val < curr_node.value:
+                if curr_node.left:
+                    curr_node = curr_node.left
+                else:
+                    curr_node.left = new_node
+                    self._size += 1
+                    break
+            else:
+                break
+
+    def search(self, val):
+        """Return the node with value val or return None."""
+        start = self.root
+        while True:
+            if start.value == val:
+                return start
+            elif val < start.value and start.left:
+                start = start.left
+            elif val > start.value and start.right:
+                start = start.right
+            else:
+                return None
+
+    def size(self):
+        """Return the integer size of the BST."""
+        return self._size
+
+    def __len__(self):
+        """Return integer size of the BST."""
+        return self.size()
+
+    def depth(self, start=''):
+        """Return the integer depth of the BST."""
+        def depth_wrapped(start):
+            if start is None:
+                return 0
+            else:
+                return max(depth_wrapped(start.right), depth_wrapped(start.left)) + 1
+        if start is '':
+            return depth_wrapped(self.root)
+        else:
+            return depth_wrapped(start)
+
+    def contains(self, val):
+        """Return True if node with value val is in BST, False if not."""
+        try:
+            return self.search(val).value == val
+        except AttributeError:
+            return False
+
+    def balance(self):
+        """Return positive or negative integer that represents how well balanced the tree is."""
+        if self.root is None:
+            return 0
+        return self.depth(self.root.right) - self.depth(self.root.left)
+
+    def in_order(self):
+        """Return a generator of the tree in in_order order."""
+        start = self.root
+        if start is None:
+            raise StopIteration
+        s = Stack()
+        while len(s) or start:
+            if start:
+                s.push(start)
+                start = start.left
+            else:
+                start = s.pop()
+                yield start.value
+                start = start.right
+
+    def pre_order(self):
+        """Return a generator of the tree in pre_order order."""
+        start = self.root
+        if start is None:
+            raise StopIteration
+        s = Stack()
+        s.push(start)
+        while len(s):
+            curr = s.pop()
+            yield curr.value
+            if curr.right is not None:
+                s.push(curr.right)
+            if curr.left is not None:
+                s.push(curr.left)
+
+    def post_order(self):
+        """Return a generator of the tree in post_order order."""
+        start = self.root
+        if start is None:
+            raise StopIteration
+        s = []
+        last = None
+        while s or start:
+            if start:
+                s.append(start)
+                start = start.left
+            else:
+                peek = s[-1]
+                if peek.right and last is not peek.right:
+                    start = peek.right
+                else:
+                    yield peek.value
+                    last = s.pop()
+
+    def breadth_first(self):
+        """Return a generator of the tree in breadth first traversal order."""
+        start = self.root
+        if start is None:
+            raise StopIteration
+        q = Queue()
+        q.enqueue(start)
+
+        while len(q) > 0:
+            current = q.dequeue()
+            yield current.value
+
+            if current._has_children():
+                for child in current._return_children():
+                    q.enqueue(child)
---
+++ src/test_bst.py
@@ -0,0 +1,227 @@
+"""This module tests a binary search tree."""
+
+import pytest
+
+
+MEDIUM_TREE = [10, 15, 13, 7, 5, 8, 3]
+
+
+@pytest.fixture
+def e_tree():
+    """Initialize an empty search tree."""
+    from bst import BinarySearchTree
+    return BinarySearchTree()
+
+
+@pytest.fixture
+def m_tree():
+    """Initialize a search tree of medium complexity."""
+    from bst import BinarySearchTree
+    b = BinarySearchTree()
+    for val in MEDIUM_TREE:
+        b.insert(val)
+    return b
+
+
+def test_init_empty_bst(e_tree):
+    """Test initializing an empty tree."""
+    from bst import BinarySearchTree
+    assert isinstance(e_tree, BinarySearchTree)
+
+
+def test_insert_first_node_sets_root(e_tree):
+    """Test inserting first node sets root."""
+    e_tree.insert(1)
+    assert e_tree.root.value == 1
+
+
+def test_insert_second_node_adds_child_correctly(e_tree):
+    """Test inserting a second node adds child appropriately."""
+    e_tree.insert(10)
+    e_tree.insert(15)
+    assert e_tree.root.right.value == 15
+    assert e_tree.root.left is None
+
+
+def test_insert_third_node_adds_child_correctly(e_tree):
+    """Test inserting a third node adds child appropriately."""
+    e_tree.insert(10)
+    e_tree.insert(15)
+    e_tree.insert(5)
+    assert e_tree.root.left.value == 5
+    assert e_tree.root.right.value == 15
+    assert e_tree.root.value == 10
+
+
+def test_find_total_depth_empty(e_tree):
+    """Test finding depth from empty tree."""
+    # import pdb; pdb.set_trace()
+    assert e_tree.depth() == 0
+
+
+def test_find_depth_one_node(e_tree):
+    """Test finding depth from a one node tree."""
+    e_tree.insert(10)
+    assert e_tree.depth() == 1
+
+
+def test_find_depth_two_node(e_tree):
+    """Test finding depth from a two node tree."""
+    e_tree.insert(10)
+    e_tree.insert(15)
+    assert e_tree.depth() == 2
+
+
+def test_find_depth_balanced_three_node(e_tree):
+    """Test finding depth from a balanced three node tree."""
+    e_tree.insert(10)
+    e_tree.insert(15)
+    e_tree.insert(5)
+    assert e_tree.depth() == 2
+
+
+def test_find_depth_unbalanced_three_node(e_tree):
+    """Test finding depth from an unbalanced three node tree."""
+    e_tree.insert(10)
+    e_tree.insert(15)
+    e_tree.insert(20)
+    assert e_tree.depth() == 3
+
+
+def test_depth_medium_tree(m_tree):
+    """Test balance of a medium sized tree."""
+    assert m_tree.depth() == 4
+    m_tree.insert(1)
+    assert m_tree.depth() == 5
+    m_tree.insert(17)
+    assert m_tree.depth() == 5
+
+
+def test_balance_empty(e_tree):
+    """Test balance of empty tree."""
+    assert e_tree.balance() == 0
+
+
+def test_balance_one_node(e_tree):
+    """Test balance of one node tree."""
+    e_tree.insert(10)
+    assert e_tree.balance() == 0
+
+
+def test_balance_two_node(e_tree):
+    """Test finding blance from a two node tree."""
+    e_tree.insert(10)
+    e_tree.insert(15)
+    assert e_tree.balance() == 1
+
+
+def test_find_balance_balanced_three_node(e_tree):
+    """Test finding balance from a balanced three node tree."""
+    e_tree.insert(10)
+    e_tree.insert(15)
+    e_tree.insert(5)
+    assert e_tree.balance() == 0
+
+
+def test_find_balance_unbalanced_three_node(e_tree):
+    """Test finding balance from an unbalanced three node tree."""
+    e_tree.insert(10)
+    e_tree.insert(15)
+    e_tree.insert(20)
+    assert e_tree.balance() == 2
+
+
+def test_find_neg_balance_unbalanced_three_node(e_tree):
+    """Test finding balance of an unbalanced three node tree."""
+    e_tree.insert(20)
+    e_tree.insert(15)
+    e_tree.insert(10)
+    assert e_tree.balance() == -2
+
+
+def test_find_balance_med_tree(m_tree):
+    """Test finding the balance of a medium tree."""
+    assert m_tree.balance() == -1
+    m_tree.insert(1)
+    assert m_tree.balance() == -2
+    m_tree.insert(12)
+    assert m_tree.balance() == -1
+
+
+def test_search_medium_tree_returns_true(m_tree):
+    """Test searching medium tree returns node."""
+    root_result = m_tree.search(10)
+    assert root_result.value == 10
+    left_result = m_tree.search(3)
+    assert left_result.value == 3
+    right_result = m_tree.search(13)
+    assert right_result.value == 13
+
+
+def test_search_medium_tree_returns_false(m_tree):
+    """Test searching medium tree returns None."""
+    assert m_tree.search(20) is None
+    assert m_tree.search(0) is None
+
+
+def test_contains_returns_true(m_tree):
+    """Test contains returns true for medium tree."""
+    assert m_tree.contains(10) is True
+    assert m_tree.contains(3) is True
+    assert m_tree.contains(15) is True
+    m_tree.insert(1)
+    assert m_tree.contains(1) is True
+
+
+def test_contains_returns_false(m_tree):
+    """Test contains returns false for medoum tree."""
+    assert m_tree.contains(27) is False
+
+
+def test_traversals_empty_tree(e_tree):
+    """Test breadth first traversal on empty and small trees."""
+    new = e_tree.breadth_first()
+    with pytest.raises(StopIteration):
+        next(new)
+    new_post = e_tree.post_order()
+    with pytest.raises(StopIteration):
+        next(new_post)
+    new_pre = e_tree.pre_order()
+    with pytest.raises(StopIteration):
+        next(new_pre)
+    new_in = e_tree.in_order()
+    with pytest.raises(StopIteration):
+        next(new_in)
+
+
+def test_breadth_first_m_tree(m_tree):
+    """Test the breadth_first method returns a generator of values in correct order."""
+    expected = [10, 7, 15, 5, 8, 13, 3]
+    gen_test = m_tree.breadth_first()
+    for i in range(len(m_tree)):
+        assert next(gen_test) == expected[i]
+
+
+def test_in_order_m_tree(m_tree):
+    """Test the breadth_first method returns a generator of values in correct order."""
+    expected = [3, 5, 7, 8, 10, 13, 15]
+    gen_test = m_tree.in_order()
+    for i in range(len(m_tree)):
+        assert next(gen_test) == expected[i]
+
+
+def test_pre_order_m_tree(m_tree):
+    """Test the breadth_first method returns a generator of values in correct order."""
+    expected = [10, 7, 5, 3, 8, 15, 13]
+    gen_test = m_tree.pre_order()
+    for i in range(len(m_tree)):
+        test_this = next(gen_test)
+        assert test_this == expected[i]
+
+
+def test_post_order_m_tree(m_tree):
+    """Test the breadth_first method returns a generator of values in correct order."""
+    expected = [3, 5, 8, 7, 13, 15, 10]
+    gen_test = m_tree.post_order()
+    for i in range(len(m_tree)):
+        assert next(gen_test) == expected[i]
