6a9d41aeb223d62458409d019102e192ea3d13d6
Colin Lamont <colinlamont@gmail.com>
2017-01-23 15:47:00 -0800

    adding more tests.

--- src/test_balanced_bst.py
+++ src/test_balanced_bst.py
@@ -6,10 +6,10 @@ BST_ROT_2_R_1 = [3, 2]
 BST_ROT_2_L_3 = [1, 2]
 BST_ROT_4_R_1 = [5, 6, 4, 2]
 BST_ROT_4_LR_2 = [5, 6, 3, 1]
-BST_ROT_4_R_12 = [6, 8, 5, 10]
+BST_ROT_4_L_12 = [6, 8, 5, 10]
 BST_ROT_4_RL_9 = [6, 8, 5, 10]
 BST_ROT_5_L_GC_6 = [2, 1, 4, 3, 5]
-BST_ROT_5_RL_GC_5 = [2, 1, 4, 3, 6]
+BST_ROT_5_L_GC_5 = [2, 1, 4, 3, 6]
 # BST_ROT_5_R_GC_1 = []
 # BST_ROT_5_LR_GC_3 = []
 
@@ -54,9 +54,9 @@ def filled_bst_rot_4_lr_2():
 
 
 @pytest.fixture
-def filled_bst_rot_4_r_12():
-    """Fixture for a 4 node tree for a right rotation with insertion of 12."""
-    new_tree = BinarySearchTree(BST_ROT_4_R_12)
+def filled_bst_rot_4_l_12():
+    """Fixture for a 4 node tree for a left rotation with insertion of 12."""
+    new_tree = BinarySearchTree(BST_ROT_4_L_12)
     return new_tree
 
 
@@ -75,9 +75,9 @@ def filled_bst_rot_5_l_gc_6():
 
 
 @pytest.fixture
-def filled_bst_rot_5_rl_gc_5():
-    """Fixture for a 4 node tree for a right-left rotation with insertion of 9."""
-    new_tree = BinarySearchTree(BST_ROT_5_RL_GC_5)
+def filled_bst_rot_5_l_gc_5():
+    """Fixture for a 4 node tree for a left rotation with insertion of 9."""
+    new_tree = BinarySearchTree(BST_ROT_5_L_GC_5)
     return new_tree
 
 
@@ -95,17 +95,21 @@ def filled_bst_rot_5_rl_gc_5():
 #     return new_tree
 
 
-def test_simple_3_node_right_rotation():
-    """Balance bst via right rotation, when adding 1 to tree."""
-    a = filled_bst_rot_2_r_1.insert(1)
+def test_simple_3_node_right_rotation(filled_bst_rot_2_r_1):
+    """Balance bst via right rotation, when adding 1 to tree of 3, 2."""
+    a = filled_bst_rot_2_r_1
+    a.insert(1)
     assert a.root.value == 2
     assert a.root.right.value == 3
     assert a.root.left.value == 1
 
 
-def test_simple_3_node_left_rotation():
-    """Balance bst via right rotation, when adding 1 to tree."""
-    a = filled_bst_rot_2_l_3.insert(3)
+def test_simple_3_node_left_rotation(filled_bst_rot_2_l_3):
+    """Balance bst via right rotation, when adding 3 to tree of 1, 2."""
+    a = filled_bst_rot_2_l_3
+    a.insert(3)
     assert a.root.value == 2
     assert a.root.right.value == 3
     assert a.root.left.value == 1
+
+# def test_left_right
