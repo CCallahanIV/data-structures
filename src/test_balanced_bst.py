6c3ad0170061d0e79c14bef0ce23755531866f00
Colin Lamont <colinlamont@gmail.com>
2017-01-23 15:26:26 -0800

    small test changes.

--- src/test_balanced_bst.py
+++ src/test_balanced_bst.py
@@ -1,5 +1,5 @@
 """Test Module for Balanced Binary Search Tree."""
-from bst import BinarySearchTree
+from balanced_bst import BinarySearchTree
 import pytest
 
 BST_ROT_2_R_1 = [3, 2]
@@ -24,11 +24,13 @@ BST_ROT_5_RL_GC_5 = [2, 1, 4, 3, 6]
 # filled_bst_rot_5_r_gc_1
 # filled_bst_rot_5_lr_gc_3
 
+
 @pytest.fixture
 def filled_bst_rot_2_r_1():
     """Fixture for a 2 node tree for a right rotation with insertion of 1."""
     new_tree = BinarySearchTree(BST_ROT_2_R_1)
     return new_tree
+
 
 @pytest.fixture
 def filled_bst_rot_2_l_3():
@@ -36,11 +38,13 @@ def filled_bst_rot_2_l_3():
     new_tree = BinarySearchTree(BST_ROT_2_L_3)
     return new_tree
 
+
 @pytest.fixture
 def filled_bst_rot_4_r_1():
     """Fixture for a 4 node tree for a right rotation with insertion of 1."""
     new_tree = BinarySearchTree(BST_ROT_4_R_1)
     return new_tree
+
 
 @pytest.fixture
 def filled_bst_rot_4_lr_2():
@@ -48,11 +52,13 @@ def filled_bst_rot_4_lr_2():
     new_tree = BinarySearchTree(BST_ROT_4_LR_2)
     return new_tree
 
+
 @pytest.fixture
 def filled_bst_rot_4_r_12():
     """Fixture for a 4 node tree for a right rotation with insertion of 12."""
     new_tree = BinarySearchTree(BST_ROT_4_R_12)
     return new_tree
+
 
 @pytest.fixture
 def filled_bst_rot_4_rl_9():
@@ -60,11 +66,13 @@ def filled_bst_rot_4_rl_9():
     new_tree = BinarySearchTree(BST_ROT_4_RL_9)
     return new_tree
 
+
 @pytest.fixture
 def filled_bst_rot_5_l_gc_6():
     """Fixture for a 4 node tree for a right-left rotation with insertion of 9."""
     new_tree = BinarySearchTree(BST_ROT_5_L_GC_6)
     return new_tree
+
 
 @pytest.fixture
 def filled_bst_rot_5_rl_gc_5():
@@ -72,19 +80,32 @@ def filled_bst_rot_5_rl_gc_5():
     new_tree = BinarySearchTree(BST_ROT_5_RL_GC_5)
     return new_tree
 
-@pytest.fixture
-def filled_bst_rot_5_r_gc_1():
-    """Fixture for a 4 node tree for a right-left rotation with insertion of 9."""
-    new_tree = BinarySearchTree(BST_ROT_5_R_GC_1)
-    return new_tree
 
-@pytest.fixture
-def filled_bst_rot_5_lr_gc_3():
-    """Fixture for a 4 node tree for a right-left rotation with insertion of 9."""
-    new_tree = BinarySearchTree(BST_ROT_5_LR_GC_3)
-    return new_tree
+# @pytest.fixture
+# def filled_bst_rot_5_r_gc_1():
+#     """Fixture for a 4 node tree for a right-left rotation with insertion of 9."""
+#     new_tree = BinarySearchTree(BST_ROT_5_R_GC_1)
+#     return new_tree
+
+
+# @pytest.fixture
+# def filled_bst_rot_5_lr_gc_3():
+#     """Fixture for a 4 node tree for a right-left rotation with insertion of 9."""
+#     new_tree = BinarySearchTree(BST_ROT_5_LR_GC_3)
+#     return new_tree
+
 
 def test_simple_3_node_right_rotation():
     """Balance bst via right rotation, when adding 1 to tree."""
     filled_bst_rot_2_r_1
-    assert()
+    assert filled_bst_rot_2_l_3.root == 2
+    assert filled_bst_rot_2_l_3.root.right == 3
+    assert filled_bst_rot_2_l_3.root.left == 1
+
+
+def test_simple_3_node_right_rotation():
+    """Balance bst via right rotation, when adding 1 to tree."""
+    filled_bst_rot_2_l_3
+    assert filled_bst_rot_2_l_3.root == 2
+    assert filled_bst_rot_2_l_3.root.right == 3
+    assert filled_bst_rot_2_l_3.root.left == 1
