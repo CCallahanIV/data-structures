4353e610de1e8cdff7eebbfa2fb4e76b68a747c3
pasaunders <kaorti@gmail.com>
2017-01-23 13:31:55 -0800

    first attempt additive and fnv hash.

---
+++ src/hash.py
@@ -0,0 +1,81 @@
+"""Hashing functions."""
+
+
+class AddHash(object):
+    """
+    Hash using a basic additive function.
+
+    __init__: set initial state of empty hash table.
+
+    get: return a value attached to the given key.
+
+    set: add a value to the table at the given key.
+
+    _hash: additive hash on a key value to return a number as a key.
+    """
+
+    def __init__(self, slots):
+        """Set starting table values."""
+        self.slots = slots
+        self.table = {}
+        for i in range(slots):
+            self.table[i] = []
+
+    def get(self, key):
+        """Return a value stored with given key."""
+        return self.table[ord(key)]
+
+    def set(self, key, val):
+        """Store val at key in the table."""
+        if not isinstance(key, str):
+            raise TypeError('key must be type str')
+        self.table[(self._hash(key) % self.slots)].append(val)
+        return None
+
+    def _hash(self, key):
+        hash_total = 0
+
+        for letter in key:
+            hash_total += ord(letter)
+
+        return hash_total
+
+
+class FNVHash(object):
+    """
+    Hash using a Fowler/Noll/Vo function.
+
+    __init__: set initial state of empty hash table.
+
+    get: return a value attached to the given key.
+
+    set: add a value to the table at the given key.
+
+    _hash: additive hash on a key value to return a number as a key.
+    """
+
+    def __init__(self, slots):
+        """Set starting table values."""
+        self.slots = slots
+        self.table = {}
+        for i in range(slots):
+            self.table[i] = []
+
+    def get(self, key):
+        """Return a value stored with given key."""
+        return self.table[ord(key)]
+
+    def set(self, key, val):
+        """Store val at key in the table."""
+        if not isinstance(key, str):
+            raise TypeError('key must be type str')
+        self.table[(self._hash(key) % self.slots)].append(val)
+        return None
+
+    def _hash(self, key):
+        hash_total = 2166136261
+
+        for letter in key:
+            hash_total = (hash_total * 16777619) ^ ord(letter)
+
+        return hash_total
