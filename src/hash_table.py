329d538d859247d79d25d7d7a7ba9e7e5d330813
William Benjamin Shields <ben@Williams-MacBook-Pro.local>
2017-01-23 22:50:40 -0800

    initial implementation of hashtable. created test file.

--- src/hash_table.py
+++ src/hash_table.py
@@ -1,5 +1,6 @@
+
 # HASH TABLE (HT)
-# 
+#
 # CodeFellows 401d5
 # Submission Date:
 #
@@ -23,3 +24,38 @@ The user can quit the program from Main Menu, and
 may return to Main Menu at any time.
 """
 
+
+class HashTable(object):
+    """Hash Table."""
+
+    """get(key) - should return the value stored with the given key"""
+    """set(key, val) - should store the given val using the given key"""
+    """_hash(key) - should hash the key provided (note that this is an internal api)"""
+
+    def __init__(self):
+        """Init function for the Hash Table class."""
+        self._container = []
+        self._num_buckets = 1000
+        for i in range(0, self._num_buckets):
+            self._container.append([])
+
+    def get(self, key):
+        """Return the value corresponding to this key in the hash table."""
+        hashed_value = self._hash(key)
+        for each in self._container[hashed_value]:
+            if each[0] == key:
+                return each[1]
+        return 'Key not in hash table.'
+
+    def set(self, key, val):
+        """Place the key value pair in the hash table."""
+        hashed_value = self._hash(key)
+        (self._container[hashed_value]).append((key, val))
+        return
+
+    def _hash(self, key):
+        """Docstring."""
+        num = 0
+        for each in key:
+            num += ord(each)
+        return num % self._num_buckets
