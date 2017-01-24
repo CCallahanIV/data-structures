9c430138330f0bc25d0b8d54dd6290a4df22b441
William Benjamin Shields <ben@Williams-MacBook-Pro.local>
2017-01-23 21:41:56 -0800

    added file for hash_table.py

---
+++ src/hash_table.py
@@ -0,0 +1,25 @@
+# HASH TABLE (HT)
+# 
+# CodeFellows 401d5
+# Submission Date:
+#
+# Authors:  Colin Lamont <https://github.com/chamberi>
+#           Ben Shields <https://github.com/iamrobinhood12345>
+#
+# URL:
+
+
+"""
+The Mailroom module allows the user to
+track donations and format emails for donors.
+The user can write tailored thank you emails
+given donor names and donation amounts.
+The user can create a report that is
+a list of donor names and their corresponding donation
+histories, arranged by in order of the total amount
+donated.
+The user can add a donor name to this list.
+The user can quit the program from Main Menu, and
+may return to Main Menu at any time.
+"""
+
