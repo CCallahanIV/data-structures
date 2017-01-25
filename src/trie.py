4dce459b0fe88a6ed6f8e4f1780f26d5f902f5cf
pasaunders <kaorti@gmail.com>
2017-01-25 13:19:53 -0800

    initial commit: trie branch

---
+++ src/trie.py
@@ -0,0 +1,33 @@
+"""A trie data structure implemented as a class."""
+
+
+class Trie(object):
+    """
+    Trie class.
+
+    __init__:
+    insert(string): adds a string to the trie.
+    contains(string): returns true if string is in the trie, else false.
+    size(): returns the number of words in the trie. 0 if empty.
+    remove(string): will remove the string from the trie. Exception otherwise.
+    """
+
+    def __init__(self):
+        """Initialize the Trie class."""
+        pass
+
+    def insert(self, string):
+        """Insert string into the trie."""
+        pass
+
+    def contains(self, string):
+        """Return a boolean, true if the string is present, else false."""
+        pass
+
+    def size(self):
+        """Return the number of strings in the trie."""
+        pass
+
+    def remove(self, string):
+        """Remove a string from the trie. Exception if string is absent."""
+        pass
