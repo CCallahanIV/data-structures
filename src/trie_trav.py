599feb00b39205dda04331f49c6cd623d65b556b
William Benjamin Shields <ben@Williams-MacBook-Pro.local>
2017-01-26 15:59:21 -0800

    beginning implementation of traversal of trie.

---
+++ src/trie_trav.py
@@ -0,0 +1,105 @@
+"""Module for Trie tree with traversal."""
+
+# TRIE TREE TRAVERSAL (TTT)
+#
+# CodeFellows 401d5
+# Submission Date:
+#
+# Authors:  Colin Lamont <https://github.com/chamberi>
+#           Ben Shields <https://github.com/iamrobinhood12345>
+#
+# URL:
+
+from collections import OrderedDict
+
+
+class Node(object):
+    """Node Class contains a value and a dictionary of nodes."""
+
+    def __init__(self, val=None):
+        """Initialize the Node class with val and empty nodes dictionary."""
+        self.val = val
+        self.nodes = OrderedDict()
+
+
+class Trie(object):
+    """Trie class, which is the Trie tree."""
+
+    """
+        insert(self, string): will insert the input string into the trie. If character in the input string is already present, it will be ignored.
+        contains(self, string): will return True if the string is in the trie, False if not.
+        size(self): will return the total number of words contained within the trie. 0 if empty.
+        remove(self, string): will remove the given string from the trie. If the word doesn’t exist, will raise an appropriate exception.
+    """
+
+    def __init__(self):
+        """Initialize the Trie class with root Node with ('*') and size of 0."""
+        self.root = Node('*')
+        self._size = 0
+
+    def insert(self, word):
+        """Insert method, which takes a word and inserts each letter of the word into the Trie, with pointer to next Node or $ if end.""" 
+        node = self.root
+        new_node = None
+        new_word = False
+        for each in word:
+            if each in node.nodes:
+                node = node.nodes[each]
+                continue
+            new_word = True
+            new_node = Node(each)
+            node.nodes[each] = new_node
+            node = new_node
+        if new_word:
+            self._size += 1
+            node.nodes['$'] = None
+
+    def contains(self, word):
+        """The contains method returns True if the word is found in the Trie tree, or False if not."""
+        node = self.root
+        for each in word:
+            if each in node.nodes:
+                node = node.nodes[each]
+            else:
+                return False
+        if '$' in node.nodes:
+            return True
+        return False
+
+    def size(self):
+        """The size method returns the number of words in the Trie."""
+        return self._size
+
+    def remove(self, word):
+        """The remove method removes the word from the Trie."""
+        node_list = []
+        node = self.root
+        for each in word:
+            if each in node.nodes:
+                node_list.append(node.nodes[each])
+                node = node.nodes[each]
+        last = node_list.pop()
+        if '$' not in last.nodes:
+            return
+        del last.nodes['$']
+        for i in range(len(node_list)):
+            last_val = last.val
+            last = node_list.pop()
+            if '$' in last.nodes:
+                self._size -= 1
+                break
+            if len(last.nodes) > 1:
+                del last.nodes[last_val]
+                self._size -= 1
+                break
+            del last.nodes[last_val]
+
+    def traveral(self, start=None):
+        node = self.root
+        output = []
+        for each in start:
+            if each in node.nodes:
+                node = node.nodes[each]
+        for each in node.nodes:
+            trav_node = None
+            for each 
