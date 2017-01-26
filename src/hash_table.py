14cf4b7b1e1fb4c5c20ced0619bb136ecfeb43ef
Colin Lamont <colinlamont@gmail.com>
2017-01-25 16:15:28 -0800

    added readme and deleted corpse code.

--- src/hash_table.py
+++ src/hash_table.py
@@ -1,4 +1,4 @@
-
+"""Hash table module."""
 # HASH TABLE (HT)
 #
 # CodeFellows 401d5
@@ -19,6 +19,10 @@ The hashing function maps the content of the key to
 a specific bucket. As the content of the key always
 maps to a specific hash value, the key's value may
 be accessed quickly.
+
+    - get(key) - should return the value stored with the given key
+    - set(key, val) - should store the given val using the given key
+    - _hash(key) - should hash the key provided (note that this is an internal api)
 """
 
 
@@ -86,59 +90,6 @@ class HashTable(object):
     #     a_sum = sum([(each[0] << next(sieve)) * (each[1] << next(sieve)) for each in ords])
     #     return a_sum % self._num_buckets
 
-# def genPrimes():
-#     def isPrime(n):
-#         if n % 2 == 0: return n == 2
-#         d = 3
-#         while d * d <= n:
-#             if n % d == 0: return False
-#             d += 2
-#         return True
-#     def init(): # change to Sieve of Eratosthenes
-#         ps, qs, sieve = [], [], [True] * 50000
-#         p, m = 3, 0
-#         while p * p <= 100000:
-#             if isPrime(p):
-#                 ps.insert(0, p)
-#                 qs.insert(0, p + (p-1) / 2)
-#                 m += 1
-#             p += 2
-#         for i in xrange(m):
-#             for j in xrange(qs[i], 50000, ps[i]):
-#                 sieve[j] = False
-#         return m, ps, qs, sieve
-#     def advance(m, ps, qs, sieve, bottom):
-#         for i in xrange(50000): sieve[i] = True
-#         for i in xrange(m):
-#             qs[i] = (qs[i] - 50000) % ps[i]
-#         p = ps[0] + 2
-#         while p * p <= bottom + 100000:
-#             if isPrime(p):
-#                 ps.insert(0, p)
-#                 qs.insert(0, (p*p - bottom - 1)/2)
-#                 m += 1
-#             p += 2
-#         for i in xrange(m):
-#             for j in xrange(qs[i], 50000, ps[i]):
-#                 sieve[j] = False
-#         return m, ps, qs, sieve
-#     m, ps, qs, sieve = init()
-#     bottom, i = 0, 1
-#     yield 2
-#     while True:
-#         if i == 50000:
-#             bottom = bottom + 100000
-#             m, ps, qs, sieve = advance(m, ps, qs, sieve, bottom)
-#             i = 0
-#         elif sieve[i]:
-#             yield bottom + i + i + 1
-#             i += 1
-#         else: i += 1
-
-# Sieve of Eratosthenes
-# Code by David Eppstein, UC Irvine, 28 Feb 2002
-# http://code.activestate.com/recipes/117119/
-
 
 def gen_primes():
     """ Generate an infinite sequence of prime numbers.
@@ -149,27 +100,32 @@ def gen_primes():
     # number being tested.
     #
     D = {}
-    
+
     # The running integer that's checked for primeness
     q = 2
-    
+
     while True:
         if q not in D:
             # q is a new prime.
             # Yield it and mark its first multiple that isn't
             # already marked in previous iterations
-            # 
+            #
             yield q
             D[q * q] = [q]
         else:
             # q is composite. D[q] is the list of primes that
             # divide it. Since we've reached q, we no longer
-            # need it in the map, but we'll mark the next 
+            # need it in the map, but we'll mark the next
             # multiples of its witnesses to prepare for larger
             # numbers
-            # 
+            #
             for p in D[q]:
                 D.setdefault(p + q, []).append(p)
             del D[q]
-        
+
         q += 1
+
+# Gen Primes function:
+# Sieve of Eratosthenes
+# Code by David Eppstein, UC Irvine, 28 Feb 2002
+# http://code.activestate.com/recipes/117119/
