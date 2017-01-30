426e59603f6e8f925dc3170b396df1b6e54705c8
Colin Lamont <colinlamont@gmail.com>
2017-01-30 14:34:02 -0800

    initial insertion sort.

--- src/insertion_sort.py
+++ src/insertion_sort.py
@@ -1,1 +1,13 @@
 """Insertion Sort Module."""
+
+
+def insert_sort(isl):
+    """Insertion sort method."""
+    for item in range(len(isl)):
+        x = isl[item]
+        neighbor = item - 1
+        while neighbor >= 0 and isl[neighbor] > x:
+            isl[neighbor + 1] = isl[neighbor]
+            neighbor -= 1
+        isl[neighbor + 1] = x
+    return isl
