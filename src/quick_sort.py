b72bf6c21f905a1557885e58e90216cc4f75b39c
Colin Lamont <colinlamont@gmail.com>
2017-02-01 14:40:19 -0800

    Added quick sort tests, coverage, docstrings, timeit to README, added timeit to quick_sort.

--- src/quick_sort.py
+++ src/quick_sort.py
@@ -11,7 +11,7 @@
 # URL:
 
 """
-quick_sort:
+quick_sort: picks a pivot, compares the rest of list next to the pivot, creates a lesser and greater list, and recursively calls quick_sort, combining the sorted lists and initial pivot at the end.
 """
 
 
@@ -33,39 +33,39 @@ def quick_sort(sort_list):
     return sort_list1 + sort_list2
 
 
-# def _random_list():
-#     """Return a list of random numbers from 0 to 300 of size 150."""
-#     import random
-#     b = random
-#     return b.sample(range(0, 300), 150)
+def _random_list():
+    """Return a list of random numbers from 0 to 300 of size 150."""
+    import random
+    b = random
+    return b.sample(range(0, 300), 150)
 
 
-# a = _random_list()
-# r = a[:]
-# b = sorted(a)
-# w = b[::-1]
+a = _random_list()
+r = a[:]
+b = sorted(a)
+w = b[::-1]
 
 
-# if __name__ == "__main__":
-#     import timeit
+if __name__ == "__main__":
+    import timeit
 
-#     random_quick_sort_timed = timeit.repeat(stmt="quick_sort(r)", setup="from quick_sort import quick_sort, r", number=1000, repeat=3)
-#     random_average_quick_sort_timed = float(sum(random_quick_sort_timed) / len(random_quick_sort_timed))
+    random_quick_sort_timed = timeit.repeat(stmt="quick_sort(r)", setup="from quick_sort import quick_sort, r", number=1000, repeat=3)
+    random_average_quick_sort_timed = float(sum(random_quick_sort_timed) / len(random_quick_sort_timed))
 
-#     print("number of runs: " + str(3))
-#     print("random quick_sort_timed: " + str(random_quick_sort_timed))
-#     print("average: ", str(random_average_quick_sort_timed))
+    print("number of runs: " + str(3))
+    print("random quick_sort_timed: " + str(random_quick_sort_timed))
+    print("average: ", str(random_average_quick_sort_timed))
 
-#     best_quick_sort_timed = timeit.repeat(stmt="quick_sort(b)", setup="from quick_sort import quick_sort, b", number=1000, repeat=3)
-#     best_average_quick_sort_timed = float(sum(best_quick_sort_timed) / len(best_quick_sort_timed))
+    arranged_quick_sort_timed = timeit.repeat(stmt="quick_sort(b)", setup="from quick_sort import quick_sort, b", number=1000, repeat=3)
+    arranged_average_quick_sort_timed = float(sum(arranged_quick_sort_timed) / len(arranged_quick_sort_timed))
 
-#     print("number of runs: " + str(3))
-#     print("best case quick_sort_timed: " + str(best_quick_sort_timed))
-#     print("average: ", str(best_average_quick_sort_timed))
+    print("number of runs: " + str(3))
+    print("arranged case quick_sort_timed: " + str(arranged_quick_sort_timed))
+    print("average: ", str(arranged_average_quick_sort_timed))
 
-#     worst_quick_sort_timed = timeit.repeat(stmt="quick_sort(w)", setup="from quick_sort import quick_sort, w", number=1000, repeat=3)
-#     worst_average_quick_sort_timed = float(sum(worst_quick_sort_timed) / len(worst_quick_sort_timed))
+    reversed_quick_sort_timed = timeit.repeat(stmt="quick_sort(w)", setup="from quick_sort import quick_sort, w", number=1000, repeat=3)
+    reversed_average_quick_sort_timed = float(sum(reversed_quick_sort_timed) / len(reversed_quick_sort_timed))
 
-#     print("number of runs: " + str(3))
-#     print("worst case quick_sort_timed: " + str(worst_quick_sort_timed))
-#     print("average: ", str(worst_average_quick_sort_timed))
+    print("number of runs: " + str(3))
+    print("reversed case quick_sort_timed: " + str(reversed_quick_sort_timed))
+    print("average: ", str(reversed_average_quick_sort_timed))
