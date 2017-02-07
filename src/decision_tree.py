97ebfdbc063dee208d177fd6d07ac2ca6798a56e
Colin Lamont <colinlamont@gmail.com>
2017-02-07 11:18:54 -0800

    initial structure for decsion tree.

---
+++ src/decision_tree.py
@@ -0,0 +1,19 @@
+"""Module for decision treee."""
+
+# DECISION TREE (MS)
+#
+# CodeFellows 401d5
+# Submission Date:
+#
+# Authors:  Colin Lamont <https://github.com/chamberi>
+#           Ben Shields <https://github.com/iamrobinhood12345>
+#
+# URL:
+
+"""
+Decision Tree Class:
+clf.fit(self, data): construct a decision tree based on some incoming data set; returns nothing
+clf.predict(self, data): returns labels for your test data.
+max_depth: limits the maximum number of steps your tree can take down any decision chain.
+min_leaf_size: Limits the minimum number of data points that may exist within a region before ending a decision chain.
+"""
