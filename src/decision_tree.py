0af3de616020ed3f4239d76055a3e918397f2e8d
William Benjamin Shields <ben@Williams-MacBook-Pro.local>
2017-02-08 12:07:01 -0800

    pseudo code for interlacing functions.

--- src/decision_tree.py
+++ src/decision_tree.py
@@ -10,8 +10,16 @@
 #
 # URL:
 
-import pandas as pd
-from bst import BinarySearchTree
+
+class TreeNode(object):
+    """An individual node for a decision tree."""
+
+    def __init__(self, column=None, split=None, left=None, right=None, data_idx=None):
+        self.column = column
+        self.split = split
+        self.left = left
+        self.right = right
+        self.data_idx = data_idx
 
 
 class DTC(object):
@@ -23,12 +31,20 @@ class DTC(object):
     min_leaf_size: Limits the minimum number of data points that may exist within a region before ending a decision chain.
     """
 
-    def __init__(self, max_depth=None, min_leaf_size=None):
-        """Docstring."""
-        self._classifier = BinarySearchTree()
+    def __init__(self, max_depth=None, min_leaf_size=1):
+        """Initialize the DTC object."""
+        self.max_depth = max_depth
+        self.min_leaf_size = min_leaf_size
+        self.root = None
 
     def fit(self, data):
-        """Docstring."""
+        """Generate conditions for classification of flowers based on training set."""
+        self.root = TreeNode(data_idx=data.index)
+        until max depth or min leaf min_leaf_size
+        split nodes starting at root
+
+
+
         pl_list = []
         pw_list = []
         for each in data:
@@ -36,55 +52,57 @@ class DTC(object):
         for each in data:
             pw_list.append(each[1])
         t = None
+        axis = None
         data_left = []
         data_right = []
         min_g = None
-        # import pdb; pdb.set_trace()
-        for i in range(int(max(pl_list))):
+        for each in pl_list:
             if t is None:
-                t = i
+                t = each
                 continue
             for j in range(len(data)):
-                if pl_list[j] < i:
+                if pl_list[j] < each:
                     data_left.append(data[j])
                 else:
                     data_right.append(data[j])
             if len(data_left) == 0 or len(data_right) == 0:
                 continue
-            g = self.G(data, data_left, data_right)
+            g = self._min_func(data, data_left, data_right)
             if min_g is None:
                 min_g = g
             if g < min_g:
                 min_g = g
-                t = i
+                t = each
+                axis = 'x'
             data_left = []
             data_right = []
-        for i in range(int(max(pw_list))):
+        for each in pw_list:
             if t is None:
-                t = i
+                t = each
                 continue
             for j in range(len(data)):
-                if pl_list[j] < i:
+                if pl_list[j] < each:
                     data_left.append(data[j])
                 else:
                     data_right.append(data[j])
             if len(data_left) == 0 or len(data_right) == 0:
                 continue
-            g = self.G(data, data_left, data_right)
+            g = self._min_func(data, data_left, data_right)
             if min_g is None:
                 min_g = g
             if g < min_g:
                 min_g = g
-                t = i
+                t = each
+                axis = 'y'
             data_left = []
             data_right = []
-        return t
+        return t, axis
 
-    def G(self, total_data, data_left, data_right):
+    def _min_func(self, total_data, data_left, data_right):
         """Docstring."""
-        return (len(data_left) / len(total_data)) * self.H(data_left) + (len(data_right) / len(total_data)) * self.H(data_right)
+        return (len(data_left) / len(total_data)) * self._analyze_purities(data_left) + (len(data_right) / len(total_data)) * self._analyze_purities(data_right)
 
-    def H(self, data):
+    def _analyze_purities(self, data):
         """Docstring."""
         setosa = []
         versicolor = []
@@ -95,8 +113,20 @@ class DTC(object):
                 versicolor.append(each)
         return (len(setosa) / len(data)) * (1 - (len(setosa) / len(data))) + (len(versicolor) / len(data)) * (1 - (len(versicolor) / len(data)))
 
+    def _split(self, node):
+        """Given some input node containing data, find best column to split on, and assign split point, and child nodes."""
+        column_name = self.some_best_column_algorithm()
+        split_pt = self.some_best_split_point_algorithm()
+        if result of splitting produces nodes with at least one value:
+            node.left = TreeNode(data_idx=node.data_idx where less than split_pt)
+            node.right = TreeNode(data_idx=node.data_idx where greater than split_pt)
+        elif left has one value or left is purely one label:
+            end left
+        elif right has one value or right is purely one label:
+            end right
+
     def predict(self, data):
-        """Docstring."""
+        """Return the likely classification for a flower(s) given petal length and petal width."""
         return_list = []
         for each in data:
             if each[0] < 2.5:
@@ -104,3 +134,10 @@ class DTC(object):
             else:
                 return_list.append("versicolor")
         return return_list
+
+
+###########################################################################
+
+        def log_scalar(num_array):
+            top = np.log10(num_array) - min(np.log10(num_array))
+            bottom = max(np.log10(num_array)) - min(np.log10(num_array))
