4bb18c8f3947014f3e960183cdcd549fbd27ea24
pasaunders <kaorti@gmail.com>
2017-02-21 14:48:27 -0800

    sketching out program structure

--- src/kmeans.py
+++ src/kmeans.py
@@ -15,9 +15,44 @@ class KMeansClassifier(object):
         if k < 0 or k > len(data):
             raise ValueError("K must a positive integer less than the length of data.")
 
+        centriods = randomCentroids(k)
+
+        iteration = 0
+        old_centroids = None
+
+        while not _should_stop(old_centroids, centroids, iteration):
+            old_centroids = centroids
+            iteration += 1
+
+            labels = _classify(data, centroids)
+
+            centroids = _assign_centroids(data, labels, k)
+
         self.fitted = True
         pass
 
     def predict(self, data):
         """Predict the class of given test data after fit."""
+        if self.fitted == False:
+            raise RuntimeError('Run KMeansClassifier.fit before running predict')
+        pass
+
+    def _calc_distance(self, pt1, pt2):
+        """Calculate the distance between two points."""
+        pass
+
+    def _classify(self, data, centroids):
+        """Assign each datapoint to the nearest centroid."""
+        pass
+
+    def _find_mean(self, points):
+        """Find the mean coordinates of points."""
+        pass
+
+    def _assign_centroids(self, data, labels, k):
+        """Assign centriod coordinates based on distance to member points."""
+        pass
+
+    def _should_stop(self, old_centroids, centroids, iteration):
+        """Determine if the fit should stop runnng."""
         pass
