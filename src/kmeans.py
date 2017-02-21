c17a1193a87ce0e0ae0e0c5a64bae42666a47b06
Ted Callahan <CCallahanIV@gmail.com>
2017-02-21 15:51:06 -0800

    Wrote Calc distance, copied in additions from Julien and Rick, wrote unit test for _calc_distance.

--- src/kmeans.py
+++ src/kmeans.py
@@ -1,45 +1,50 @@
 """Implementation of the K-Means Classifier."""
+import random
+from math import sqrt
 
 
 class KMeansClassifier(object):
     """Implementation of the K-Means Classifier."""
 
-    def __init__(self, max_iter=None, min_step=None):
+    def __init__(self, max_iter=5, min_step=None):
         """Initialize a K-Means Classifier object."""
         self.max_iter = max_iter
         self.min_step = min_step
         self.fitted = False
+        self.centroids = None
 
     def fit(self, data, k=2):
         """Fit K centroids to given data."""
         if k < 0 or k > len(data):
             raise ValueError("K must a positive integer less than the length of data.")
 
-        centriods = randomCentroids(k)
+        centroids = self._random_centroids(k)
 
         iteration = 0
         old_centroids = None
 
-        while not _should_stop(old_centroids, centroids, iteration):
+        while not self._should_stop(old_centroids, centroids, iteration):
             old_centroids = centroids
             iteration += 1
 
-            labels = _classify(data, centroids)
+            labels = self._classify(data, centroids)
 
-            centroids = _assign_centroids(data, labels, k)
+            centroids = self._assign_centroids(data, labels, k)
 
         self.fitted = True
-        pass
 
     def predict(self, data):
         """Predict the class of given test data after fit."""
-        if self.fitted == False:
+        if self.fitted is False:
             raise RuntimeError('Run KMeansClassifier.fit before running predict')
         pass
 
     def _calc_distance(self, pt1, pt2):
         """Calculate the distance between two points."""
-        pass
+        dist = 0.0
+        for i in range(len(pt1) - 2):
+            dist += (pt1[i] - pt2[i])**2
+        return sqrt(dist)
 
     def _classify(self, data, centroids):
         """Assign each datapoint to the nearest centroid."""
@@ -47,7 +52,10 @@ class KMeansClassifier(object):
 
     def _find_mean(self, points):
         """Find the mean coordinates of points."""
-        pass
+        col_means = []
+        for column in points:
+            col_means.append(column.mean)
+        return col_means
 
     def _assign_centroids(self, data, labels, k):
         """Assign centriod coordinates based on distance to member points."""
@@ -56,3 +64,10 @@ class KMeansClassifier(object):
     def _should_stop(self, old_centroids, centroids, iteration):
         """Determine if the fit should stop runnng."""
         pass
+
+    def _random_centroids(self, data, k):
+        """Return randomly generated centroids."""
+        k_list = []
+        for i in range(k):
+            k_list.append([random.uniform(min(data[column]), max(data[column])) for column in range(len(data))])
+        return k_list
--- src/test_kmeans.py
+++ src/test_kmeans.py
@@ -1,1 +1,19 @@
 """Tests for the K-Means Classifier."""
+
+from math import sqrt
+import pandas as pd
+import pytest
+
+
+@pytest.fixture
+def kmc():
+    """Fixture to return a default KMC."""
+    from kmeans import KMeansClassifier
+    return KMeansClassifier()
+
+
+def test_calc_distance(kmc):
+    """Test the _calc_distance method of the K Means Classifier."""
+    rows = [[2, 2, 1], [0, 0, 1]]
+    data = pd.DataFrame(data=rows, columns=['x', 'y', 'class'])
+    assert kmc._calc_distance(data.loc[0], data.loc[1]) == sqrt(8)
