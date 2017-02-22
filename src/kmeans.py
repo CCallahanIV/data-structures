ea79357e1b41793353ca8b5a4c52caf72cc3f340
pasaunders <kaorti@gmail.com>
2017-02-21 18:07:01 -0800

    added tests and troubleshooting

--- src/kmeans.py
+++ src/kmeans.py
@@ -16,58 +16,76 @@ class KMeansClassifier(object):
     def fit(self, data, k=2):
         """Fit K centroids to given data."""
         if k < 0 or k > len(data):
-            raise ValueError("K must a positive integer less than the length of data.")
+            raise ValueError("K must be a positive integer less than the length of data.")
 
-        centroids = self._random_centroids(k)
-
+        data['group'] = None
+        self.centroids = self._random_centroids(data, k)
         iteration = 0
         old_centroids = None
 
-        while not self._should_stop(old_centroids, centroids, iteration):
-            old_centroids = centroids
+        while not self._should_stop(old_centroids, iteration, k):
+            old_centroids = self.centroids
             iteration += 1
-
-            labels = self._classify(data, centroids)
-
-            centroids = self._assign_centroids(data, labels, k)
-
+            self._classify(data)
+            self.centroids = self._assign_centroids(data, k)
         self.fitted = True
 
     def predict(self, data):
         """Predict the class of given test data after fit."""
         if self.fitted is False:
-            raise RuntimeError('Run KMeansClassifier.fit before running predict')
-        pass
+            raise RuntimeError('Run KMeansClassifier.fit before running predict.')
+        self._classify(data)
+        return data
 
     def _calc_distance(self, pt1, pt2):
         """Calculate the distance between two points."""
         dist = 0.0
+        print('distance called from: ', pt1, pt2)
         for i in range(len(pt1) - 2):
+            print('i:', i, 'pt1:', pt1[i], 'pt2:', pt2[i])
             dist += (pt1[i] - pt2[i])**2
         return sqrt(dist)
 
-    def _classify(self, data, centroids):
+    def _classify(self, data):
         """Assign each datapoint to the nearest centroid."""
-        pass
+        for point in data.iterrows():
+            print('point: ', point)
+            distances = []
+            for cent in self.centroids:
+                print('cent', cent)
+                distances.append(self._calc_distance(cent, point))
+            point.group = distances.index(min(distances))
 
     def _find_mean(self, points):
         """Find the mean coordinates of points."""
         col_means = []
         for column in points:
-            col_means.append(column.mean)
+            col_means.append(points[column].mean())
         return col_means
 
-    def _assign_centroids(self, data, labels, k):
-        """Assign centriod coordinates based on distance to member points."""
-        pass
+    def _assign_centroids(self, data, k):
+        """Set centroid coordinates to mean of their assigned datapoints."""
+        groups = []
+        for _ in range(k):
+            groups.append(data[data["groups"] == k])
+        for idx, group in enumerate(groups):
+            self.centroids[idx] = self._find_mean(group)
 
-    def _should_stop(self, old_centroids, centroids, iteration):
+    def _should_stop(self, old_centroids, centroids, iteration, k):
         """Determine if the fit should stop runnng."""
-        pass
+        if iteration > self.max_iter:
+            return True
+        centroid_movements = []
+        for i in range(k):
+            centroid_movements.append(self._calc_distance(old_centroids[i], centroids[i]))
+        if self.min_step:
+            if max(centroid_movements) < self.min_step:
+                return True
+        return False
 
     def _random_centroids(self, data, k):
         """Return randomly generated centroids."""
         k_list = []
         for i in range(k):
-            k_list.append([random.uniform(min(data[column]), max(data[column])) for column in range(len(data))])
+            k_list.append([random.uniform(min(data[column]), max(data[column])) for column in range(len(data) - 2)])
         return k_list
--- src/test_kmeans.py
+++ src/test_kmeans.py
@@ -3,6 +3,7 @@
 from math import sqrt
 import pandas as pd
 import pytest
+import numpy as np
 
 
 @pytest.fixture
@@ -12,8 +13,53 @@ def kmc():
     return KMeansClassifier()
 
 
-def test_calc_distance(kmc):
+@pytest.fixture
+def some_data():
+    """Fixture to return some dummy data."""
+    data = np.array([[2, 3], [4, 5], [6, 7], [8, 9], [1, 1], [2, 2], [3, 3], [4, 4]])
+    return pd.DataFrame(data=data)
+
+
+def test_calc_distance_rows(kmc):
     """Test the _calc_distance method of the K Means Classifier."""
-    rows = [[2, 2, 1], [0, 0, 1]]
-    data = pd.DataFrame(data=rows, columns=['x', 'y', 'class'])
+    rows = [[2, 2, 1, 0], [0, 0, 1, 0]]
+    data = pd.DataFrame(data=rows, columns=['x', 'y', 'class', 'dummy'])
     assert kmc._calc_distance(data.loc[0], data.loc[1]) == sqrt(8)
+
+
+def test_calc_distance(kmc):
+    """Test distance calculator helper method."""
+    assert kmc._calc_distance([0, 0, 0, 0], [3, 4, 0, 0]) == 5.0
+
+
+def test_find_mean(kmc, some_data):
+    """Unit test for find mean."""
+    data_means = kmc._find_mean(some_data)
+    assert data_means == [3.75, 4.25]
+
+
+def test_should_stop_false_max_iter(kmc):
+    """Test that a kmc does not doesnt stop iterating, with only a max iter of 5 set."""
+    old_centroids = [[2, 3], [7, 8]]
+    new_centroids = [[2, 4], [90, 55]]
+    assert not kmc._should_stop(old_centroids, new_centroids, 3, 2)
+
+
+def test_should_stop_true_max_iter(kmc):
+    """Test that a kmc does not doesnt stop iterating, with only a max iter of 5 set."""
+    old_centroids = [[2, 3], [7, 8]]
+    new_centroids = [[2, 4], [90, 55]]
+    assert kmc._should_stop(old_centroids, new_centroids, 6, 2)
+
+
+def test_classify():
+    """Unit test for classifying datapoints."""
+    from kmeans import KMeansClassifier
+    clusters = KMeansClassifier()
+    rows = [[4, 4, 1, 0], [0, 0, 1, 0]]
+    data = pd.DataFrame(data=rows, columns=['x', 'y', 'class', 'group'])
+    clusters.centroids = None
+    clusters.centroids = [[4, 5, 0, 0], [0, 1, 0, 0]]
+    # import pdb; pdb.set_trace()
+    clusters._classify(data)
+    assert True
